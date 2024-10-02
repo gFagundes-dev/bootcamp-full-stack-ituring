0ecbf99ddde64ecd3003d58b8de49effdc7d67bf
gFagundes-dev <guilhermeap.067@gmail.com>
2024-10-02 17:40:01 -0300

    Aula Pratica 5: pratica-5.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,8 @@
+// Pratica da Aula 5 - Mensagem Whastsaap
+const nome = "Guilherme Aparecido";
+const email = "gfagundes@belpit.com.br";
+const profissao = "Estagiario";
+const idade = 24;
+const lugarSonhaConhecer = "Suiça";
+
+console.log(mensagem="Olá, "+nome +"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " +lugarSonhaConhecer +". Confirma pra gente alguns dados? A sua idade é " +idade +", você é  "+profissao +" e o seu email é " +email +" ? Ficamos no aguardo. Muito obrigado e boa viagem! :D");
