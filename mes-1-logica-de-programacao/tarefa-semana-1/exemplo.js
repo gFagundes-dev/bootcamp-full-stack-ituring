bab131642e831ed698edff71bc7c7eb7076f17b5
gFagundes-dev <guilhermeap.067@gmail.com>
2024-10-02 08:41:53 -0300

    Adicionado o código - exemplo.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/exemplo.js
@@ -0,0 +1,24 @@
+//Manipulacao de dados - Variaveis
+//Strings (texto)
+
+// Cria as caixas de entrada das variaveis / ex: "remetente"
+// Coloca o valor dentro da caixa / ex: "remetente" = "e-mail"       
+// let - variavel que pode mudar de valor
+let remetente = "gfagundes@belpit.com.br"
+let destinatio = "ftristante@belpit.com.br"
+
+// const - variavel ao qual nao muda o valor
+const idade = 24
+
+let assunto = "Manipulacao de variaveis - Estou online no bootcamp"
+let corpo = "Tudo bem? Estou realizando a pratica de manipulacao de variaveis atraves do curso bootcamp"
+
+        // Exibe o valor no termimal
+console.log(remetente)
+console.log(destinatio)
+console.log(idade)
+console.log(assunto)
+console.log(corpo)
+
+
+
