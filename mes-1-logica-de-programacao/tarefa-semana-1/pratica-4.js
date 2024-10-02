66cc60336b00a1c99f037ff98a4ff242f8da914b
gFagundes-dev <guilhermeap.067@gmail.com>
2024-10-02 17:49:28 -0300

    Resolvendo conflitos

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-4.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-4.js
@@ -6,12 +6,16 @@ const distanciaMilhasLuaTerra = 238900;
 const quilometrosEmMilhas = 1.6093; 
 
 // Realizando a funcao
-function valorConversaoMilhas(fatorConversao){
-      fatorConversao = distanciaMilhasLuaTerra * quilometrosEmMilhas; // Calculo utilizando as variaveis
-      return fatorConversao; // Retornando o valor
+function valorConversaoMilhas(fatorConversao){    
+    fatorConversao = distanciaMilhasLuaTerra * quilometrosEmMilhas; // Calculo utilizando as variaveis
+    return fatorConversao; // Retornando o valor
 }
 
-// Exibindo os valores 
+//Exibindo os valores 
 console.log("Distancia em milhas: "+ distanciaMilhasLuaTerra);
 console.log("Valor de 1 milha em km: "+quilometrosEmMilhas);
-console.log("Conversao de Milha/KM: "+ valorConversaoMilhas());
+console.log("Conversao de Milha/KM: "+ valorConversaoMilhas());  
+
+
+
+
