39fd058115bfd2088fcc167fb61a57f485b2f3df
gFagundes-dev <guilhermeap.067@gmail.com>
2024-10-02 14:50:50 -0300

    Adicionado a Aula Pratica 3 - pratica-3.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,16 @@
+// Verificando se a variavel é verdadeira
+
+// Valores de cada caixa
+const nome = "Guilherme Fagundes";
+const email = "gfagundes@belpit.com.br";
+const profissao = "Estagiario";
+const idade = 24
+const lugarSongaConhecer = "Suiça"
+
+
+// Verificacao do tipo variavel - string
+console.log(typeof nome == "string");
+console.log(typeof email == "string");
+console.log(typeof profissao == "string");
+console.log(typeof idade == "string"); // Obs.: O tipo da variavel é number
+console.log(typeof lugarSongaConhecer == "string");
